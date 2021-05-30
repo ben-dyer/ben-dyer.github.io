66f19586fa6927afc889bdf72c3a6f624f4b111d
Ben Dyer <43922444+ben-dyer@users.noreply.github.com>
2021-05-31 01:02:25 +0900

    Add gatsby transformer remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,24 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
